8c3c2779d978c945506bfca240863e03cbb6bdb2
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-08-10 22:40:13 -0300

    fix: arrumado botao de download do pdf

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,7 +1,10 @@
 function downloadCv() {
     let link = document.createElement("a");
     link.download = "juanmartinscaitano_cv";
-    link.href = "./assets/banner_bambushop.pdf";
+    link.href = "../../../assets/banner_bambushop.pdf";
     link.click();
     link.remove();
+    
 }
+
+// fetch("../../../assets/banner_bambushop.pdf").then(console.log)
